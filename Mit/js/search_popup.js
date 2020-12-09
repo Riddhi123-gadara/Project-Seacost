42d4e8be5c03d8fd1b3470fc7b7e8eb290f206b9
mitkpatel <mitkpatel09@gmail.com>
2020-12-09 18:53:36 +0530

    FinalProject-Mit

--- Mit/js/search_popup.js
+++ Mit/js/search_popup.js
@@ -1,21 +1,4 @@
-// Code for search box 
-function search_item() { 
-  let search = document.getElementById('searchItem').value;          //Get the text from box 
-  search = search.toLowerCase();                                     //Convert into lower case
-  let temp = document.getElementsByClassName('product-item');        //Search for div with class-name
-    
-  for (i = 0; i < temp.length; i++) {                                //Loop seatch until the length not exceed
-      if (!temp[i].innerHTML.toLowerCase().includes(search)) { 
-        temp[i].style.display="none";                                //If not found then display nothing
-      } 
-      else { 
-        temp[i].style.display="flex";                                //Else display Item flex  
-      } 
-  } 
-} 
-
-
-//Code for popup menu with JQuery
+// Code for popup menu
 let openModalPopup = document.querySelectorAll('[data-modal-target]');
 let closeModalPopup = document.querySelectorAll('[data-close-button]');
 
@@ -44,3 +27,20 @@ function closeModal(modal) {
     return false;
   modal.classList.remove('active');
 }
+
+// Code for search box 
+function search_item() { 
+  let search = document.getElementById('searchItem').value;          //Get the text from box 
+  search = search.toLowerCase();                                     //Convert into lower case
+  let temp = document.getElementsByClassName('product-item');        //Search for div with class-name
+    
+  for (i = 0; i < temp.length; i++) {                                //Loop seatch until the length not exceed
+      if (!temp[i].innerHTML.toLowerCase().includes(search)) { 
+        temp[i].style.display="none";                                //If not found then display nothing
+      } 
+      else { 
+        temp[i].style.display="flex";                                //Else display Item flex  
+      } 
+  } 
+} 
+
